5d59b24c1f7aa393c95811796213bfb1c1330b51
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2025-01-10 12:40:46 +0100

    f

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,34 @@
-import Navbar from "@/components/Layout/Navbar";
-import Hero from "@/components/hero";
-import Testimonials from "@/components/testimonials";
-import FeaturesSlider from "@/components/featuresslider";
-import Footer from "@/components/Layout/Footer";
-import TrustBadges from "@/components/trustbadges";
-import FAQ from "@/components/faq";
-import Why from "@/components/why";
-import TieredSystem from "@/components/tieredsystem";
-import About from "@/components/about";
-import Free from "@/components/free";
-import ClassContent from "@/components/classcontent";
-import Articles from "@/components/articles";
-import Calls from "@/components/calls";
-import Contents from "@/components/contents";
-import { Element } from "react-scroll";
-import Meta from "@/components/Layout/Meta";
-import StudentTests from "@/components/studenttests";
-import WistiaVideo from "@/components/wistia-embed";
+import Navbar from '@/components/Layout/Navbar'
+import Hero from '@/components/hero'
+import Testimonials from '@/components/testimonials'
+import FeaturesSlider from '@/components/featuresslider'
+import Footer from '@/components/Layout/Footer'
+import TrustBadges from '@/components/trustbadges'
+import FAQ from '@/components/faq'
+import Why from '@/components/why'
+import TieredSystem from '@/components/tieredsystem'
+import About from '@/components/about'
+import Free from '@/components/free'
+import ClassContent from '@/components/classcontent'
+import Articles from '@/components/articles'
+import Calls from '@/components/calls'
+import Contents from '@/components/contents'
+import { Element } from 'react-scroll'
+import Meta from '@/components/Layout/Meta'
+import StudentTests from '@/components/studenttests'
+import WistiaVideo from '@/components/wistia-embed'
 
 export default function Home() {
-  return (
-    <main style={{background: "linear-gradient(338deg, rgba(3,2,32,1) 0%, rgba(30,12,119,1) 49%, rgba(8,5,82,1) 100%)"}}>
-      <Meta />
+	return (
+		<main
+			style={{
+				background:
+					'linear-gradient(338deg, rgba(3,2,32,1) 0%, rgba(30,12,119,1) 49%, rgba(8,5,82,1) 100%)',
+			}}
+		>
+			{/**
+       * 
+       *       <Meta />
       <Navbar />
       <Hero />
       <div className="w-[90%] mx-auto max-w-4xl">
@@ -32,8 +39,7 @@ export default function Home() {
                  <Element name="testimonials">
         <Testimonials />
       </Element>
-         */}
-      <TrustBadges />
+            <TrustBadges />
       <About />
       <Element name="omos">
         <Articles />
@@ -46,6 +52,8 @@ export default function Home() {
       <Why />
       <FAQ />
       <Footer />
-    </main>
-  );
+         */}
+			<p>siden er under opdatering</p>
+		</main>
+	)
 }
