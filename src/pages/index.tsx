95f8a4cc549b7e0ebd7702e9a006850359422011
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2025-01-12 12:28:43 +0100

    f

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,34 +26,30 @@ export default function Home() {
 					'linear-gradient(338deg, rgba(3,2,32,1) 0%, rgba(30,12,119,1) 49%, rgba(8,5,82,1) 100%)',
 			}}
 		>
-			{/**
-       * 
-       *       <Meta />
-      <Navbar />
-      <Hero />
-      <div className="w-[90%] mx-auto max-w-4xl">
-        <WistiaVideo />
-      </div>
-      <FeaturesSlider />
-           {/*
-                 <Element name="testimonials">
-        <Testimonials />
-      </Element>
-            <TrustBadges />
-      <About />
-      <Element name="omos">
-        <Articles />
-      </Element>
-      <Calls />
-      <Contents />
-      <Element name="content">
-        <ClassContent />
-      </Element>
-      <Why />
-      <FAQ />
-      <Footer />
-         */}
-			<p>siden er under opdatering</p>
+			* * <Meta />
+			<Navbar />
+			<Hero />
+			<div className="w-[90%] mx-auto max-w-4xl">
+				<WistiaVideo />
+			</div>
+			<FeaturesSlider />
+			/*
+			<Element name="testimonials">
+				<Testimonials />
+			</Element>
+			<TrustBadges />
+			<About />
+			<Element name="omos">
+				<Articles />
+			</Element>
+			<Calls />
+			<Contents />
+			<Element name="content">
+				<ClassContent />
+			</Element>
+			<Why />
+			<FAQ />
+			<Footer />
 		</main>
 	)
 }
